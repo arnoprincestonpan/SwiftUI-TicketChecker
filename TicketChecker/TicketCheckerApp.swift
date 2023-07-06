be1c3816ffd00189d6908e69233632e83b3aaab2
ARNO PAN <apan11@my.bcit.ca>
2023-07-06 12:43:23 -0700

    Initial Commit

---
+++ TicketChecker/TicketCheckerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicketCheckerApp.swift
+//  TicketChecker
+//
+//  Created by Arno Princeston pan on 2023-07-06.
+//
+
+import SwiftUI
+
+@main
+struct TicketCheckerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
